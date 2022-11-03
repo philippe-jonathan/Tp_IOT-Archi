b4df977de021b3ba341539e135fb5287561db633
jonathan <philippe1jonathan@gmail.com>
2022-11-03 15:15:23 +0100

    Update index.js

--- pulseur/index.js
+++ pulseur/index.js
@@ -20,14 +20,29 @@ connection();
 function getRandomTemp(min, max) {
   return Math.random() * (max - min) + min;
 }
-    
+
 setInterval(async () => {
+  // Init used data
   let random = Math.round(getRandomTemp(-10, 40))+"°C";
   let data = {
     'temp': random,
     'sensor_id' : Math.round(getRandomTemp(10000, 100000))
   };
-  await client.set('value', JSON.stringify(data));
-  // res.send("New value added");
-  console.log("New value added: "+ random);
+
+  // Send to redis DB
+  let data_send = JSON.stringify(data);
+  
+  // Filter data in redis DB
+  let json = JSON.parse(data_send);
+  if(json.sensor_id && json.temp) {
+    console.log("value type is OK");
+    await client.set('value', data_send);
+    console.log("New value added: "+ random);
+  } else {
+    console.log("ERROR value type in database");
+  }
+
 }, 1000);
+
+// Check data in two db (requete SQL et redis pour comparer)
+// let current_value = await client.get('value');
