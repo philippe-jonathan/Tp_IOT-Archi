aaf8d7f29a753b2d8508d98ee07b8128fef39b8b
jonathan <philippe1jonathan@gmail.com>
2022-11-17 08:36:42 +0100

    maj

--- pulseur/index.js
+++ pulseur/index.js
@@ -5,20 +5,39 @@ const redis = require('redis');
 
 // NOTE - REDIS
 const client = redis.createClient({
-    url: 'redis://redis-server',
-    port: 6379
-  });
+  url: 'redis://redis-server',
+  port: 6379
+});
 
 async function redis_connection() {
-    client.connect(function(err) {
-        if(err) throw err;
-        console.log("Redis database connected!")
-    })
+  client.connect(function(err) {
+    if(err) throw err;
+    console.log("Redis database connected!")
+  })
 }
 // --------------------
 
 // NOTE - Redis connection
 redis_connection();
+
+// NOTE - BROKER (Mosquirtto)
+var ascoltatori = require('ascoltatori');
+var settings = {
+  type: 'mqtt',
+  json: false,
+  mqtt: require('mqtt'),
+  url: 'mqtt://localhost:8883'
+};
+
+ascoltatori.build(settings, function (err, ascoltatore) {
+  
+  // NOTE - Subscribes to a "datas" topic
+  ascoltatore.subscribe('datas', function() {
+   console.log(arguments);
+   // { '0': 'datas', '1': 'Datas send to local database (redis)' }
+ });
+
+});
 
 function getRandomTemp(min, max) {
   return Math.random() * (max - min) + min;
@@ -44,12 +63,29 @@ setInterval(async () => {
   if(json.sensor_id && json.temp) {
     console.log("value type is OK");
     await client.set(getTimestamp(), data_send);
+    // NOTE - PUBLISHER
+    ascoltatori.build(settings, function (err, ascoltatore) { 
+      // NOTE - Publishes a message to the topic 'datas'
+      ascoltatore.publish('datas', 'Datas send to local database (redis)', function() {
+        console.log('Publish work!');
+      });
+    });
+    // -------------------
     console.log("New value added: "+ random);
   } else {
     console.log("ERROR value type in database");
   }
 
 }, 1000);
+
+
+// var mosca = require('mosca');
+// var settings = {port: 1883};
+// var broker = new mosca.Server(settings);
+
+// broker.on('ready', () => {
+//     console.log('Broker is ready!');
+// });
 
 // Check data in two db (requete SQL et redis pour comparer)
 // let current_value = await client.get('value');
