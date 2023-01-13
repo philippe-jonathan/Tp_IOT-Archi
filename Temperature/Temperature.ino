f58e4fe442d1f3208d923a0b907cce31dc63a5d8
peutt <aymericqueret@gmail.com>
2023-01-13 16:09:53 +0100

    bluetooth fonctionnel avec bouton poussoir banche gauche:p16, droite:-

--- Temperature/Temperature.ino
+++ Temperature/Temperature.ino
@@ -1,14 +1,54 @@
 #include "DHT.h"
+#include "BluetoothSerial.h"
 
 #define DHTPIN 14
 #define DHTTYPE DHT11
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
 
+BluetoothSerial SerialBT;
 DHT dht(DHTPIN, DHTTYPE);
 int state_s1 = 0;
 float hMemory;
 float tMemory;
+const int buttonPin = 16;
+int buttonState = HIGH;
+int prev_state = buttonState;
+int current_state = buttonState;
 
-void SM_s1() {
+void SM_s1_bluetooth() {
+    // put your main code here, to run repeatedly:
+  buttonState = digitalRead(buttonPin);
+  current_state = buttonState;
+  
+  switch (current_state) {
+    case HIGH:
+      if (prev_state == LOW){
+        prev_state = HIGH;
+        SerialBT.end(); //On desactive le bluetooth
+        Serial.println("The device end, now you can't pair it with bluetooth!");
+      }
+      break;
+    case LOW:
+      if (prev_state == HIGH){
+        prev_state = LOW;
+        SerialBT.begin("OnEstLa"); //Bluetooth device name
+        Serial.println("The device started, now you can pair it with bluetooth!");
+      }
+      break;
+    default:
+      break;
+  }
+  if (Serial.available()) {
+    SerialBT.println(Serial.readString());
+  }
+  if (SerialBT.available()) {
+    Serial.write(SerialBT.read());
+  }
+}
+
+void SM_s1_temperature() {
   //Almost every state needs these lines, so I'll put it outside the State Machine
   float h = dht.readHumidity();
   // Read temperature as Celsius (the default)
@@ -37,7 +77,6 @@ void SM_s1() {
         Serial.println(F("Failed to read from DHT sensor!"));  
         return;
       }
-      float hif = dht.computeHeatIndex(f, h);
       // Compute heat index in Celsius (isFahreheit = false)
       float hic = dht.computeHeatIndex(t, h, false);
       Serial.print(F("Humidity: "));
@@ -45,12 +84,9 @@ void SM_s1() {
       Serial.print(F("%  Temperature: "));
       Serial.print(t);
       Serial.print(F("C "));
-      Serial.print(f);
-      Serial.print(F("F  Heat index: "));
+      Serial.print(F(" Heat index: "));
       Serial.print(hic);
-      Serial.print(F("C "));
-      Serial.print(hif);
-      Serial.println(F("F"));
+      Serial.println(F("C "));
       state_s1 = 1;
       break;            
   }
@@ -58,13 +94,14 @@ void SM_s1() {
 
 void setup()
 {    
-    //capteur de temperature et humidité
     Serial.begin(115200);
+    pinMode(buttonPin, INPUT_PULLUP);
     Serial.println(F("DHTxx test!"));
     dht.begin();
 }
 
 void loop()
 {
-  SM_s1();
+  SM_s1_bluetooth();
+  SM_s1_temperature();
 }
