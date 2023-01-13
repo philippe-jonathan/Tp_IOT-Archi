dff06c62b09c595a7adaaa32518b4c4e05bf2bea
jonathan <philippe1jonathan@gmail.com>
2023-01-13 14:21:55 +0100

    maj

--- airlux/lib/main.dart
+++ airlux/lib/main.dart
@@ -1,69 +1,18 @@
-import 'package:airlux/bleutooth.dart';
-import 'package:airlux/pairing.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'pages/homePage.dart';
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-      ),
-      home: const MyHomePage(title: 'Poc On/Off Light'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  ButtonStyle style =
-      ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Contrôlez la lumière avec le bouton ci-dessous!',
-            ),
-            ElevatedButton(
-              style: style,
-              onPressed: () {},
-              child: const Text('Allumer'),
-            ),
-          ],
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) {
-            return const BluetoothDeviceListPage();
-          }));
-        },
-        tooltip: 'Nouveau',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
+        home: const MyHomePage(),
+      );
 }
