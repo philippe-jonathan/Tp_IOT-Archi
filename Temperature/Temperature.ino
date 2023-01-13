82a943762eab0da99b81889abc5a878402c47a32
peutt <aymericqueret@gmail.com>
2023-01-13 15:17:18 +0100

    capteur temperature fonctionnel banche gauche:p14, droite:+, millieux:-

--- Temperature/Temperature.ino
+++ Temperature/Temperature.ino
@@ -1,22 +1,9 @@
-/*
- *  This sketch sends data via HTTP GET requests to data.sparkfun.com service.
- *
- *  You need to get streamId and privateKey at data.sparkfun.com and paste them
- *  below. Or just customize this script to talk to other HTTP servers.
- *
- */
-//capteur de temperature et humidité
 #include "DHT.h"
 
 #define DHTPIN 14
 #define DHTTYPE DHT11
+
 DHT dht(DHTPIN, DHTTYPE);
-
-
-
-
-
-
 int state_s1 = 0;
 float hMemory;
 float tMemory;
@@ -26,8 +13,6 @@ void SM_s1() {
   float h = dht.readHumidity();
   // Read temperature as Celsius (the default)
   float t = dht.readTemperature();
-  
-  int state_prev_s1 = state_s1;
 
   //State Machine Section
   switch (state_s1) {
@@ -41,15 +26,14 @@ void SM_s1() {
       if (tMemory != t || hMemory != h) {state_s1 = 2;}
     break;
     case 2: //Mesure Temperature & humidity
+      // Read humidity %
       float h = dht.readHumidity();
       // Read temperature as Celsius (the default)
       float t = dht.readTemperature();
-      // Read temperature as Fahrenheit (isFahrenheit = true)
-      float f = dht.readTemperature(true);
       tMemory=t;
       hMemory=h;
       // Check if any reads failed and exit early (to try again).
-      if (isnan(h) || isnan(t) || isnan(f)) {
+      if (isnan(h) || isnan(t)) {
         Serial.println(F("Failed to read from DHT sensor!"));  
         return;
       }
@@ -67,11 +51,10 @@ void SM_s1() {
       Serial.print(F("C "));
       Serial.print(hif);
       Serial.println(F("F"));
-      
+      state_s1 = 1;
       break;            
-    }
   }
-
+}
 
 void setup()
 {    
@@ -79,20 +62,9 @@ void setup()
     Serial.begin(115200);
     Serial.println(F("DHTxx test!"));
     dht.begin();
-    Serial.begin(115200);
 }
-
 
 void loop()
 {
-  // Wait a few seconds between measurements.
   SM_s1();
-  //delay(2000);
-
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  
-  
-  
 }
-
