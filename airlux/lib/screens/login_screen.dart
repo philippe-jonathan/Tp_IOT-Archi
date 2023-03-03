1f2f8b9ff3a9907bb786a3b0d7b505fa735b7c4a
philippe-jonathan <39793063+philippe-jonathan@users.noreply.github.com>
2023-03-03 12:00:21 +0100

    __

--- airlux/lib/screens/login_screen.dart
+++ airlux/lib/screens/login_screen.dart
@@ -21,13 +21,12 @@ class LoginScreen extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-
               // Bonjour
               Text(
                 'Connectez vous !',
                 style: Theme.of(context).textTheme.titleMedium,
-                ),
-        
+              ),
+
               const SizedBox(height: 50),
 
               // Email field
@@ -36,37 +35,36 @@ class LoginScreen extends StatelessWidget {
                 emailText: true,
                 hintText: 'E-mail',
                 obscureText: false,
-                ),
-              
+              ),
+
               const SizedBox(height: 20),
 
               // Password field
               CustomTextfield(
                 controller: passwordController,
                 emailText: false,
-                hintText: 'Mot de passe', 
+                hintText: 'Mot de passe',
                 obscureText: true,
-                ),
-        
+              ),
+
               const SizedBox(height: 20),
 
               // Forgot password
               TextButton(
-                onPressed: () {},
-                child: const Text('Mot de passe oublié')
-                ),
+                  onPressed: () {}, child: const Text('Mot de passe oublié')),
 
               const SizedBox(height: 20),
 
               // Login button
               ElevatedButton(
                 onPressed: () {
-                  Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => HomeScreen()),
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomeScreen()),
                   );
-                }, 
+                },
                 child: const Text('Connexion'),
-                ),
+              ),
 
               const SizedBox(height: 50),
               // Create account
@@ -78,10 +76,9 @@ class LoginScreen extends StatelessWidget {
                   );
                 },
                 child: const Text('Créer un compte'),
-                ),
+              ),
             ],
           ),
-          
         ),
       ),
     );
